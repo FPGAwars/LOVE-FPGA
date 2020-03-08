5abc2c2ea762a9829d9592bf339361b010689f3e
Obijuan <juan@iearobotics.com>
2020-03-08 11:38:33 +0100

    LED panel: bitvar() function implemented

--- Web-panels/01-One-LED/led-test.js
+++ Web-panels/01-One-LED/led-test.js
@@ -1,22 +1,51 @@
+//-- Identificador de la variable bit a monitorizar en el LED0
+const VARBIT0 = 'b'
 
-const sp = new SerialPanel('msg_serial', 'butSerial', serial_received2)
+//-- Obtener el panel serie
+//-- Se pasan como argumentos los identificadores HTML del mensaje de
+//-- deteccion del puerto serie y del botón de conectar
+//-- El último es la función de retrollamada de cuando se recibe un
+//-- comando
+const sp = new SerialPanel('msg_serial', 'butSerial', serial_cmd)
+
+//-- Acceder al LED0 del panel
 const led0 = new Led("led0")
 
-function serial_received2(data)
+function bitvar(cmd, varname)
+//-- Procesar el comando recibido y comprobar si es una
+//-- varaible binaria
+//-- Si es así, retorna su valor: "0", "1"
+//-- En caso contrario retorna null
 {
-  let bit = convert_bit(data);
-  led0.set(bit);
-  console.log(bit);
+    let str1 = varname + "1"
+    let str0 = varname + "0"
+
+    if (cmd == str1) {
+      return "1"
+    } else if (cmd == str0) {
+      return "0"
+    }
+
+    //-- Comando no reconocido
+    return null;
 }
 
-
-function convert_bit(value)
+//-- Funcion para procesar los comandos recibidos
+//-- El comando recibido no incluye el \n final
+//-- (el modulo serialPanel lo ha quitado)
+function serial_cmd(cmd)
 {
-  if (value == "b1\n" || value == "1") {
-    return "1"
+  //-- Degbug
+  console.log("Comand: " + cmd + " Length: " + cmd.length);
+
+  //-- Leer el bit de la variable binaria,
+  //-- si se ha recibido el comando correcto
+  let bit = bitvar(cmd, VARBIT0)
+
+  //-- Actuar sobre el LED del panel si se ha recibido el bit
+  if (bit) {
+    console.log("Bit: " + bit)
+    led0.set(bit);
   }
-  else if (value == "b0\n" || value == "0") {
-    return "0"
-  }
-  return "0";
+
 }

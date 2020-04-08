89ebd7696bb0b6015eeef1bb2f00f676b4ed8d4f
Obijuan <juan@iearobotics.com>
2020-04-08 19:39:47 +0200

    One Switch panel: Switch is disable and turned off then disconnected

--- Web-panels/One-Switch/panel.js
+++ Web-panels/One-Switch/panel.js
@@ -10,11 +10,32 @@ const sp = new SerialPanel('msg_serial', 'butSerial')
 //-- Crear el objeto switch
 sw = new Switch("sw", toggle, "click");
 
+//-- Establecer la funcion de retrollamada cuando
+//-- el puerto serie se ha abierto
+sp.onconnect = () => {
+  console.log("Debug: Panel: Conectados!!");
+
+  //-- Cambiar el estado del switch a enable
+  sw.enable()
+}
+
+sp.ondisconnect = () => {
+  console.log("Debug: Panel: Desconectar...")
+
+  //-- Apagar el switch, para que el hardware vaya
+  //-- acorde
+  sw.off();
+
+  //-- Deshabilitar el switch. El usuario ya no puede
+  //-- cambiarlo
+  sw.disable();
+}
+
 //-- Función de retrollamada del switch
 function toggle(s)
 {
   //-- Debug
-  //console.log("Toggle!!!!: " + s);
+  console.log("Toggle!!!!: " + s);
 
   //-- Enviar comando
   sp.write(SW_VAR + s + "\n");

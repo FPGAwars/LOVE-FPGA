98a777eb8aff84f147f4a505fc9ce9d0b6c53569
Obijuan <juan@iearobotics.com>
2020-04-09 11:26:38 +0200

    One Switch panel: Reset and Sync buttons added

--- Web-panels/One-Switch/panel.js
+++ Web-panels/One-Switch/panel.js
@@ -6,6 +6,11 @@ const SW_VAR = "a"
 //-- deteccion del puerto serie y del botón de conectar
 const sp = new SerialPanel('msg_serial', 'butSerial')
 
+//-- Obtener los botones de Reset y Sync
+const butReset = document.getElementById("butReset");
+const butSync = document.getElementById("butSync");
+
+
 //-- Crear el objeto switch
 sw = new Switch("sw", toggle, "click");
 
@@ -14,15 +19,26 @@ sw = new Switch("sw", toggle, "click");
 sp.onconnect = () => {
   console.log("Debug: Panel: Conectados!!");
 
+  //-- Activar los botones de Reset y Sync
+  butReset.disabled = false;
+  butSync.disabled = false;
+
   //-- Cambiar el estado del switch a enable
   sw.enable()
 
   //-- Al arrancar enviamos el estado a 0
   sw.off();
+
+  //-- Llevar el foco al boton de reset
+  butReset.focus();
 }
 
 sp.ondisconnect = () => {
   console.log("Debug: Panel: Desconectar...")
+
+  //-- Deshabilitar Los botones de Reset y Sync
+  butReset.disabled = true;
+  butSync.disabled = true;
 
   //-- Apagar el switch, para que el hardware vaya
   //-- acorde
@@ -31,6 +47,19 @@ sp.ondisconnect = () => {
   //-- Deshabilitar el switch. El usuario ya no puede
   //-- cambiarlo
   sw.disable();
+
+}
+
+//-- Funcion de retrollama del boton de RESET
+butReset.onclick = ()=> {
+  //-- Poner el switch a cero
+  sw.off();
+}
+
+//-- Función de retrollamada del botón de Sync
+butSync.onclick = () => {
+  //-- Enviar el estado actual al hardware
+  toggle(sw.get())
 }
 
 //-- Función de retrollamada del switch

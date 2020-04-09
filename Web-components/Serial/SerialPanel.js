3122399074b1520de5b2ff6bb3e0c1ea3b0b71ab
Obijuan <juan@iearobotics.com>
2020-04-09 13:18:12 +0200

    One switch panel: Testing the download option

--- Web-components/Serial/SerialPanel.js
+++ Web-components/Serial/SerialPanel.js
@@ -72,7 +72,7 @@ class SerialPanel {
       await this.disconnect();
 
       //-- Cambiar el estado de la interfaz
-      this.butSerial.textContent = '🔌Conectar';
+      this.butSerial.textContent = '🔌Connect';
 
       return;
     }
@@ -81,7 +81,7 @@ class SerialPanel {
     await this.connect();
 
     //-- Activar la interfaz
-    this.butSerial.textContent = '🔌Desconectar';
+    this.butSerial.textContent = '🔌Disconnect';
 
     //-- Llamar a la funcion de retrollamada de puerto abierto
     //-- (si estaba definida)

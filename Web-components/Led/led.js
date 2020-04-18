dbf33854b00b5acc731541aa241547ce700cacab
Obijuan <juan@iearobotics.com>
2020-04-18 13:40:46 +0200

    Web component: LED: image size increased. More methods added

--- Web-components/Led/led.js
+++ Web-components/Led/led.js
@@ -5,9 +5,42 @@ class Led {
   constructor(ledid) {
     this.element = document.getElementById(ledid)
 
+    //-- Si swid es una cadena, representa el identificador
+    //-- del boton
+    if (typeof(ledid)=="string") {
+      this.element = document.getElementById(ledid);
+      console.log("Es STRING!");
+    }  else if (typeof(ledid)=="object") {
+      this.element = ledid;
+      console.log("Es objeto!");
+    } else return;
+
+    //-- El ultimo caracter del identificador es la variable asociada
+    //-- al pulsador
+    this.varid = this.element.id[this.element.id.length-1];
+
     //-- Clase para activar led
     //-- Definida en led.css
     this.CLASS_ON="ledon";
+
+    //-- Para desactivar el led
+    this.CLASS_DISABLED = "led_disabled";
+
+  }
+
+  //-- Comprobar si el elemento esta deshabilitado
+  disabled() {
+    return this.element.classList.contains(this.CLASS_DISABLED);
+  }
+
+  //-- Activar el led
+  enable() {
+    this.element.classList.remove(this.CLASS_DISABLED);
+  }
+
+  //-- Desactivar el led
+  disable() {
+    this.element.classList.add(this.CLASS_DISABLED);
   }
 
   toggle() {
@@ -15,11 +48,13 @@ class Led {
   }
 
   on() {
-    this.element.classList.add(this.CLASS_ON);
+    if (!this.disabled())
+      this.element.classList.add(this.CLASS_ON);
   }
 
   off() {
-    this.element.classList.remove(this.CLASS_ON);
+    if (!this.disabled())
+      this.element.classList.remove(this.CLASS_ON);
   }
 
   set(state) {
